218c4655c02afa5b061bba2c501378d3b6494b34
suissemaxx <mail@tv07.com>
2021-10-28 18:17:06 +0200

    Update sf_data_streamlit_dashboard.py

--- sf_data_streamlit_dashboard.py
+++ sf_data_streamlit_dashboard.py
@@ -193,7 +193,7 @@ else:
   st.markdown("# 🌉 San Francisco Purchasing Data Dashboard")
   st.subheader(f"Data overview for purchases from {time_frame.lower()}")
   st.markdown(f"##### First available date: {start_date} | Last available date: {end_date}")
-  st.subheader(f"📆 {days_till_fiscal_end} days left till end of fiscal year.")
+  st.markdown(f"##### 📆 {days_till_fiscal_end} days left till end of fiscal year.")
   st.markdown("---")
 
   purchase_count = sf.shape[0]
